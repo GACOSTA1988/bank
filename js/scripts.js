8d2438888f3957ad2691fafee6a226e77dd74884
Student <example@example.com>
2019-11-05 15:36:43 -0800

    create functions and prototypes for adding and removing money

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,40 @@
+function Account(name, balance){
+  this.name = name,
+  this.balance = balance
+}
+
+Account.prototype.addMoney = function(money){
+  this.balance += money
+}
+Account.prototype.withdraw = function(money){
+  this.balance -= money
+}
+
+var userAccount = new Account()
+
 $(document).ready(function(){
+  console.log(userAccount);
+  $(".account").submit(function(event){
+    event.preventDefault();
+    var userName = $("#accountName").val();
+    var userDeposit = parseInt($("#initialDeposit").val());
+    userAccount = new Account(userName, userDeposit);
+    console.log(userAccount);
+  })
+  $(".addFunds").submit(function(event){
+    event.preventDefault();
+    userAccount = userAccount
+    var deposit = parseInt($("#addFunds").val());
+    userAccount.addMoney(deposit);
+    console.log(userAccount)
+  });
+$(".removeFunds").submit(function(event){
+    var withdrawal = parseInt($("#withdrawalAmmount").val());
+    console.log(deposit, withdrawal);
+    console.log(test);
+    userAccount.withdraw(withdrawal);
+    console.log(userAccount)
+  })
+
 
 });
