b30afb4865af499c67f6ee75d931783cd4b20568
Student <example@example.com>
2019-11-05 16:11:05 -0800

    make the deposit and withdraw one form

--- js/scripts.js
+++ js/scripts.js
@@ -11,11 +11,11 @@ Account.prototype.withdraw = function(money){
 }
 
 var userAccount = new Account()
-var showBalance = function(account){
-  $("#balance").html(account.balance)
-}
 
 $(document).ready(function(){
+  $("input#addFunds").val(0);
+  $("input#withdrawalAmmount").val(0);
+  $("input#initialDeposit").val(0);
   console.log(userAccount);
   $(".account").submit(function(event){
     event.preventDefault();
@@ -23,26 +23,18 @@ $(document).ready(function(){
     var userDeposit = parseInt($("#initialDeposit").val());
     userAccount = new Account(userName, userDeposit);
     console.log(userAccount);
-    showBalance(userAccount)
   })
-  $(".addFunds").submit(function(event){
+  $(".money").submit(function(event){
     event.preventDefault();
     userAccount = userAccount
     var deposit = parseInt($("#addFunds").val());
+    var withdrawal = parseInt($("#withdrawalAmmount").val());
+    console.log(deposit, withdrawal);
     userAccount.addMoney(deposit);
+    userAccount.withdraw(withdrawal);
     console.log(userAccount)
-    $("input#addFunds").val("");
-    showBalance(userAccount)
-  });
-$(".removeFunds").submit(function(event){
-  event.preventDefault();
-    userAccount = userAccount
-    var withdraw = parseInt($("#withdrawalAmmount").val());
-    userAccount.withdraw(withdraw)
-    console.log(userAccount)
-    $("input#withdrawalAmmount").val("");
-    showBalance(userAccount)
+    $("input#addFunds").val(0);
+    $("input#withdrawalAmmount").val(0);
   })
-
 
 });
