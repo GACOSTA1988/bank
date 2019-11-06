ec0cee251b5f98cc341eafa7968d491be683177c
Student <example@example.com>
2019-11-05 16:45:22 -0800

    change styling and show/hide balance

--- js/scripts.js
+++ js/scripts.js
@@ -12,29 +12,35 @@ Account.prototype.withdraw = function(money){
 
 var userAccount = new Account()
 
+var showBalance = function(account){
+  $("#balance").html(account.balance)
+}
+
 $(document).ready(function(){
   $("input#addFunds").val(0);
   $("input#withdrawalAmmount").val(0);
   $("input#initialDeposit").val(0);
-  console.log(userAccount);
   $(".account").submit(function(event){
     event.preventDefault();
     var userName = $("#accountName").val();
     var userDeposit = parseInt($("#initialDeposit").val());
     userAccount = new Account(userName, userDeposit);
-    console.log(userAccount);
+    $("#moneyManagement").show();
+    $("#accountCreation").hide();
+    $(".totalMoney").show();
+    showBalance(userAccount);
   })
   $(".money").submit(function(event){
     event.preventDefault();
     userAccount = userAccount
     var deposit = parseInt($("#addFunds").val());
     var withdrawal = parseInt($("#withdrawalAmmount").val());
-    console.log(deposit, withdrawal);
     userAccount.addMoney(deposit);
     userAccount.withdraw(withdrawal);
-    console.log(userAccount)
     $("input#addFunds").val(0);
     $("input#withdrawalAmmount").val(0);
+    showBalance(userAccount);
+    $(".totalMoney").show();
   })
 
 });
